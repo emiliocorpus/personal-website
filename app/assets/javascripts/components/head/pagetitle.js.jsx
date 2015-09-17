94aa0caf4ba5140276e1dc76f0dc5d8b0256641e
Emilio Corpus <emiliocorpus@gmail.com>
2015-09-17 11:13:08 -0700

    adds body and footer folders

--- app/assets/javascripts/components/head/pagetitle.js.jsx
+++ app/assets/javascripts/components/head/pagetitle.js.jsx
@@ -2,7 +2,7 @@ var PageTitle = React.createClass({
 	render: function() {
 		return (
 			<div id="page-title" className="debugger">
-				<h1>EMILIO CORPUS </h1>
+				<h1>EMILIO CORPUS</h1>
 			</div>
 		)
 	}
