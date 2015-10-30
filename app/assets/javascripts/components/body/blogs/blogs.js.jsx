5e8507439ddde7350b2526e62c4ba41c7fc67b77
Emilio Corpus <emiliocorpus@gmail.com>
2015-10-30 14:45:27 -0700

    adds syntax fix and removes undefined states

--- app/assets/javascripts/components/body/blogs/blogs.js.jsx
+++ app/assets/javascripts/components/body/blogs/blogs.js.jsx
@@ -11,22 +11,22 @@ var Blogs = React.createClass({
 	    dataType: 'json',
 	    cache: false,
 	    success: function(data) {
-	        this.setState({projects: data})
+	        this.setState({blogs: data})
 	    }.bind(this),
 	    error: function(xhr, status, err) {
 	      console.error('/blogs', status, err.toString());
-	      console.error(this.state.projects);
+	      console.error(this.state.blogs);
 	    }.bind(this)
 	  });
 	},
 	loadCheck:function () {
 		var blogs = this.state.blogs
 		var blogsHolder = []
-		if (this.state.projects !== undefined) {
-			blogs.map(function(index, el)) {
+		if (this.state.blogs !== undefined) {
+			blogs.map(function(index, el) {
 				var blog = < Blog data={blogs[el]} />
 				blogsHolder.push(blog)
-			}
+			})
 		}
 		return blogsHolder
 	},
@@ -38,7 +38,6 @@ var Blogs = React.createClass({
 				{loadedBlogs}
 				<br />
 			</div>
-
 		)
 	}
 })
