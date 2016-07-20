d1e56d0097e39d26482a3b108dd90e9097477b6f
emiliocorpus <emiliocorpus@gmail.com>
2016-07-20 11:35:59 -0700

    edits seed file and project order

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,17 @@
 	# 				content: Faker::Lorem.paragraph(20)
 	# 				)
 	# }
-	Project.create(name: 'Pokédex', description:"Designed with RoR and React.js, this app utilizes the FlipMove component library to animate when certain elements are removed/added to the virtual DOM. Currently only features first 151 Pokémon", url: "https://pokedex-on-react.herokuapp.com/", picture_url:'pokedex-screenshot.png')
+	Project.create(name: "FitbitGraphs", description: "Uses FitbiAPI to fetch various data like steps and calories from a users Fitbit tracker. Also uses Charts.js to interpret JSON data from the API into a viewable graph.", url: "http://www.fitbitgraphs.co", picture_url: 'fitbit-graphs-screenshot.png')
 
-	Project.create(name: 'Accounts Manager', description: 'Constructed with React.js, this application was made to better understand the interaction between state, props, and the virtual DOM in React.', url: "https://accounts-manager-on-react.herokuapp.com/", picture_url:"expense-tracker-screenshot.png")
+	Project.create(name: 'Pokédex', description:"Designed with RoR and React.js, this app utilizes the FlipMove component library to animate when React elements are removed or added to the virtual DOM. Currently only features first 151 Pokémon.", url: "https://pokedex-on-react.herokuapp.com/", picture_url:'pokedex-screenshot.png')
+
+	Project.create(name: 'Accounts Manager', description: 'Constructed with React.js, this application was created to better understand the interaction between state, props, and the virtual DOM in React.js.', url: "https://accounts-manager-on-react.herokuapp.com/", picture_url:"expense-tracker-screenshot.png")
 	
 	Project.create(name: "Battleship", description:"Made with React.js and Ruby on Rails. This game was created to further understand how to build a fully functional single-page appliction via React.js. ", url: "/portfolio/battleship", picture_url: "battleship-screenshot.png")
 
 	Project.create(name: "Guess That Word", description: "Project was also made with React.js to gain more experience in designing a known game (similar to Hangman) into a single-page app. ", url: "/portfolio/guess-that-word", picture_url: 'guess-that-screenshot.png')
 
-	Project.create(name: "FitbitGraphs", description: "Uses FitbiAPI to fetch various data like steps and calories from a users Fitbit tracker. Also uses Charts.js to interpret JSON data from the API into a viewable graph.", url: "http://www.fitbitgraphs.co", picture_url: 'fitbit-graphs-screenshot.png')
-
-	Project.create(name: 'My Website', description: 'Made with Ruby on Rails, this site will document my progress as a developer while at the same time showcase my enhanced skills along the way. Utlizes JQuery for CSS animations.', url: "http://www.emiliocorpus.com", picture_url:'website-screenshot.png')
+	Project.create(name: 'My Website', description: 'Made with Ruby on Rails, this site will document my progress as a developer while at the same time showcase my various projects along the way. Utlizes JQuery for CSS animations.', url: "http://www.emiliocorpus.com", picture_url:'website-screenshot.png')
 
 
 
