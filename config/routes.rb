0ac7cbe1d396f3212b95920c761fbcfa4a6b5ee4
emiliocorpus <emiliocorpus@gmail.com>
2016-07-20 12:19:16 -0700

    edits navbar font colors, configures routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
   root 'page#home'
 
-  get 'about' => 'page#about', as: :about
   get 'blogs' => 'page#blogs', as: :blogs
-  # get 'portfolio/'=> 'projects#index', as: :portfolio
-  # get 'resume' => 'page#resume', as: :resume
-  # get 'contact' => 'page#contact', as: :contact
+
+
 
   get 'portfolio/guess-that-word' => 'snowman#index'
   get 'portfolio/snowman/word/retrieve' => 'snowman#retrieve'
