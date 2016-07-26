1d8c731bacfe99655058afcddd9a2988141b8f30
emiliocorpus <emiliocorpus@gmail.com>
2016-07-26 13:39:11 -0700

    validate presence of pic_url

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,5 @@
 class Blog < ActiveRecord::Base
 	validates :title, presence: {:message => "cannot be blank"}
 	validates :html, presence: {:message => "content cannot be blank"}
+	validates :pic_url
 end
